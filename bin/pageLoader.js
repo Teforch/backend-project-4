c0dafa02cfd2bcabd5b154fab2f271c16b5380b3
Teforch <matveev.gleb.boy@gmail.com>
2024-12-07 23:24:13 +0300

    add debug from pageLoader.js

--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import pageLoader from '../index.js';
-import debug from 'debug';
-const log = debug('page-loader');
 
 const program = new Command();
 
@@ -13,7 +11,7 @@ program
   .arguments('<url>')
   .option('-o, --output [dir]', 'output dir', process.cwd())
   .action((url, options) => {
-    log(options.output);
+    console.log(options.output);
     pageLoader(url, options.output);
   });
 
